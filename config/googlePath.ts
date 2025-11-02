0649d020096f562bf533e8b7b99359ee808daf70
Pavlo Sambur <samburp@gmail.com>
2025-11-02 16:20:25 +0200

    c

---
+++ config/googlePath.ts
@@ -0,0 +1,3 @@
+export const bannersFolder = "1XteDpZWGMkTUnYp-GWLlI_C5TgPoW9WD";
+export const workSheetId = "16z6mg7nWxzmSxKuITcEnWprVH55GnE2rG2dE1bFG7Ok";
+export const suggestedMatchesListName = "suggested_matches";
